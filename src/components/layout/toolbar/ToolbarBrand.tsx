34e82f7c18ebb1243b9e3b5d52f765c5d705b7c8
Balázs Dévay <bdevay@gmail.com>
2025-06-09 19:30:44 +0200

    Icon path fixed

--- src/components/layout/toolbar/ToolbarBrand.tsx
+++ src/components/layout/toolbar/ToolbarBrand.tsx
@@ -5,7 +5,7 @@ const ToolbarBrand: React.FC = () => {
   return (
     <div className="flex items-center space-x-2">
       <img 
-        src="/public/icon.png" 
+        src="public/icon.png" 
         alt="LangCanvas" 
         className="w-16 h-16 sm:w-20 sm:h-20"
       />
